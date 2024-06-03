6f331e8457a73262bd378dbfcd74f59c29d8287b
Jhonny0102 <fjhon868@gmail.com>
2024-06-03 11:03:00 +0200

    Add project files.

---
+++ AWSMvcSegundaPractica/Controllers/PracticaController.cs
@@ -0,0 +1,42 @@
+﻿using AWSMvcSegundaPractica.Models;
+using AWSMvcSegundaPractica.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSMvcSegundaPractica.Controllers
+{
+    public class PracticaController : Controller
+    {
+        private ServicePractica service;
+        public PracticaController(ServicePractica service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Eventos()
+        {
+            List<Evento> eventos = await this.service.GetEventosAsync();
+            return View(eventos);
+        }
+
+        public async Task<IActionResult> Categorias()
+        {
+            List<Categoria> categorias = await this.service.GetCategoriasAsync();
+            return View(categorias);
+        }
+
+        public async Task<IActionResult> EventosCategorias()
+        {
+            List<Categoria> categorias = await this.service.GetCategoriasAsync();
+            ViewData["CATEGORIAS"] = categorias;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> EventosCategorias(int idcategoria)
+        {
+            List<Categoria> categorias = await this.service.GetCategoriasAsync();
+            ViewData["CATEGORIAS"] = categorias;
+            List<Evento> eventos = await this.service.GetEventosCategoriaAsync(idcategoria);
+            return View(eventos);
+        }
+
+    }
+}
---
+++ AWSMvcSegundaPractica/Services/ServicePractica.cs
@@ -0,0 +1,57 @@
+﻿using AWSMvcSegundaPractica.Models;
+using System.Net.Http.Headers;
+
+namespace AWSMvcSegundaPractica.Services
+{
+    public class ServicePractica
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServicePractica(IConfiguration configuration)
+        {
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiPracticaAWS");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response = await client.GetAsync(this.UrlApi + request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Evento>> GetEventosAsync()
+        {
+            string request = "api/practica/geteventos";
+            List<Evento> data = await this.CallApiAsync<List<Evento>>(request);
+            return data;
+        }
+
+        public async Task<List<Categoria>> GetCategoriasAsync()
+        {
+            string request = "api/practica/getcategorias";
+            List<Categoria> data = await this.CallApiAsync<List<Categoria>>(request);
+            return data;
+        }
+
+        public async Task<List<Evento>> GetEventosCategoriaAsync(int idcategoria)
+        {
+            string request = "api/practica/geteventoscategoria/"+idcategoria;
+            List<Evento> data = await this.CallApiAsync<List<Evento>>(request);
+            return data;
+        }
+    }
+}
